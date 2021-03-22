42e35052fc31c41c94efdceeba7c19200af77ed6
Henry Alberto Chavez Chavez <hachch1984@gmail.com>
2021-03-22 13:23:20 -0500

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
   const [pauseVideo_value, pauseVideo_setValue] = useState(false);
 
   const videoRef = useRef<any>(null);
-   
+
 
 
   const getVideo = () => {
@@ -57,21 +57,34 @@ const App = () => {
   }, [videoRef, showVideo_value, counter_value]);
 
 
- 
+
 
 
 
   return (
-    <div style={{ margin: "50px 50px" }}>
-      <div style={{ width: "300px", height: "300px", border: "solid 1px black" }}>
+
+    <div className='p-5 d-flex justify-content-center align-items-center flex-column '>
+
+      <h6 className='display-4'>Video Cam Player</h6>
+
+      <div style={{ width: "340px", height: "340px" }} className='card' >
+
         {showVideo_value &&
-          <video ref={videoRef} style={{ filter: showGrayColor_value || showBlur_value ? (showGrayColor_value ? 'grayscale(1) ' : '') + (showBlur_value ? 'blur(5px)' : '') : undefined }} />}
+
+          <div className='card-body p-3'>
+            <video ref={videoRef} style={{ filter: showGrayColor_value || showBlur_value ? (showGrayColor_value ? 'grayscale(1) ' : '') + (showBlur_value ? 'blur(5px)' : '') : undefined }}
+
+            />
+          </div>
+
+        }
 
       </div>
       <div>
 
 
         <button
+          className='btn btn-primary mr-3 mt-3'
           onClick={() => {
             counter_setValue(0);
             showGrayColor_setValue(false);
@@ -80,18 +93,18 @@ const App = () => {
 
         {showVideo_value &&
           <React.Fragment>
-            <button style={{ marginLeft: '10px' }} onClick={() => {
+            <button className='btn btn-secondary mr-3 mt-3' onClick={() => {
               showGrayColor_setValue(!showGrayColor_value);
 
             }} >{showGrayColor_value ? 'Show natural color' : 'Show gray color'}</button>
 
-            <button style={{ marginLeft: '10px' }} onClick={() => {
+            <button className='btn btn-secondary mr-3 mt-3' onClick={() => {
               showBlur_setValue(!showBlur_value);
 
             }} >{showBlur_value ? 'Hide blur efect' : 'Show Blur efect'}</button>
 
 
-            <button style={{ marginLeft: '10px' }} onClick={() => {
+            <button className='btn btn-secondary mr-3 mt-3' onClick={() => {
               if (pauseVideo_value) {
                 pauseVideo_setValue(false);
                 videoRef.current.play();
@@ -106,12 +119,21 @@ const App = () => {
         }
 
 
-        {showVideo_value && <p>counter {counter_value}</p>}
+        {showVideo_value && <p className='mt-3'>counter {counter_value} seconds</p>}
 
+
+
+        <div className='mt-3 d-flex justify-content-between align-items-center'>
+
+          <div> <span className='text-muted mr-3'>Developed by<br/>Henry Alberto Chavez Chavez</span>     </div>
+          <img style={{borderRadius:'50%'}} src='https://avatars.githubusercontent.com/u/5782038?s=48&v=4' alt='developer' />
+        </div>
 
       </div>
 
     </div>
+
+
   );
 };
 
